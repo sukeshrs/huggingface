209692ad4bb0c08d66f8db8b09d7f15e6161914d
sukeshrs <sukesrs@gmail.com>
2024-01-16 19:27:57 -0800

    added code for training

---
+++ simple_training_script.py
@@ -0,0 +1,22 @@
+import torch
+from transformers import AdamW, AutoTokenizer, AutoModelForSequenceClassification
+
+# Same as before
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+sequences = [
+    "This train goes fast",
+    "Its snowing today",
+    "The football game was okey"
+]
+batch = tokenizer(sequences, padding=True,
+                  truncation=True, return_tensors="pt")
+
+# This is new
+batch["labels"] = torch.tensor([1, 1])
+
+optimizer = AdamW(model.parameters())
+loss = model(**batch).loss
+loss.backward()
+optimizer.step()
